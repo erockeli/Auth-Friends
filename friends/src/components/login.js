d0adce869b1cc6264941d3bf92bfaf6f56e19fc9
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-12-18 09:00:47 -0600

    Created Login form

---
+++ friends/src/components/login.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const Login (props) => {
+
+
+const [creds, setCreds] = useState({username: "", password: ""})
+
+
+const handleChange = event => {
+    setCreds({ ...creds, [event.taret.name]: event.target.value});
+};
+
+const handleSubmit = event => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/api/login', creds)
+    .then(res => {console.log(res);
+        localStorage.setItem('token', resdata.payload);
+        props.history.push('/friends')
+    })
+    .catch(err => console.log(err.response))
+
+}
+
+return(
+
+
+    <form>
+        <input 
+        type='text'
+        name= 'username'
+        placeholder='username'
+        onChange={handleChange}
+        value= {creds.username}/>
+
+        <input 
+        type='password'
+        name= 'password'
+        placeholder='password'
+        onChange={handleChange}
+        value= {creds.password}/>
+        
+        
+        <button type='submit'>Submit</button>
+    </form>
+)
+
+}
+
+export default Login;
